7c016e4cb6e283b3faa2c40c0a641b301d42f316
anujkaushik1 <anujkaushik1512@gmail.com>
2025-02-17 01:35:13 +0530

    basic producer code

--- producer.js
+++ producer.js
@@ -6,19 +6,29 @@ async function main(){
     const messageData = [
         {
             name: "anuj",
-            value: "haha"
+            value: "haha",
+            location: "north"
         },
         {
             name: "test_anuj",
-            value: "hello-world"
+            value: "hello-world",
+            location: "north"
+ 
         }
     ]
 
     await producer.send({
         topic: 'rider-updates',
         messages: [
-            { key: 'key1', value: messageData  },
+            { key: 'key1', value: JSON.stringify(messageData)  },
         ],
         partition: 0 // send this message to north
     })
+    
+    console.log('Producer message sent');
+    
+
+    await producer.disconnect(); 
 }
+
+main();
